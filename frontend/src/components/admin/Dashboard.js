1ca1826622633a0d9a3c8c122827eb24e91b42a3
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-02-17 14:50:53 +0800

    BETA V1.2.2

--- frontend/src/components/admin/Dashboard.js
+++ frontend/src/components/admin/Dashboard.js
@@ -62,7 +62,7 @@ const Dashboard = () => {
                     <div className="card-body">
                       <div className="text-center card-font-size">
                         Total Amount
-                        <br /> <b>${totalAmount && totalAmount.toFixed(2)}</b>
+                        <br /> <b>₱{totalAmount && totalAmount.toFixed(2)}</b>
                       </div>
                       {/* <div className="text-center card-font-size">Total Amount<br /> <b></b>
 
