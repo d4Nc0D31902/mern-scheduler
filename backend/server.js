18ccdcdef3d5d4f6ea4fb7fa81193159e1c1acff
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-02-18 10:24:32 +0800

    test lang

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,48 @@
-const app = require('./app');
-const connectDatabase = require('./config/database')
-const path = require('path')
-const cloudinary = require('cloudinary')
+// const app = require('./app');
+// const connectDatabase = require('./config/database')
+// const path = require('path')
+// const cloudinary = require('cloudinary')
 
-require('dotenv').config({path:  './config/.env'});
+// require('dotenv').config({path:  './config/.env'});
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET
+// });
+
+// connectDatabase();
+// console.log(process.env.DATABASE)
+// if (process.env.NODE_ENV !== 'PRODUCTION')
+// 	require('dotenv').config({ path: 'backend/config/.env' })
+// app.listen(process.env.PORT, () => {
+// 	console.log(`server started on port:' ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+// });
+
+const app = require("./app");
+const connectDatabase = require("./config/database");
+const path = require("path");
+const cloudinary = require("cloudinary");
+
+require("dotenv").config({ path: "./config/.env" });
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 connectDatabase();
-console.log(process.env.DATABASE)
-if (process.env.NODE_ENV !== 'PRODUCTION') 
-	require('dotenv').config({ path: 'backend/config/.env' })
+console.log(process.env.DATABASE);
+if (process.env.NODE_ENV !== "PRODUCTION")
+  require("dotenv").config({ path: "backend/config/.env" });
+
+// Define a route handler for the root route '/'
+app.get("/", (req, res) => {
+  res.send("Hello World!"); // or any other response you want for the root route
+});
+
+// Start the server
 app.listen(process.env.PORT, () => {
-	console.log(`server started on port:' ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+  console.log(
+    `server started on port:' ${process.env.PORT} in ${process.env.NODE_ENV} mode`
+  );
 });
