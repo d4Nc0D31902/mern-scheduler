d0e6009576a9404737ccefcf8c92638e5ec048d4
d4Nc0D31902 <danielrodri1902@gmail.com>
2024-02-18 10:33:29 +0800

    undo

--- backend/app.js
+++ backend/app.js
@@ -17,12 +17,8 @@ const errorMiddleware = require("./middlewares/errors");
 app.use(express.json({ limit: "100mb" }));
 // app.set("trust proxy", 1);
 app.use(
-  // cors({
-  //   origin: "http://localhost:3000",
-  //   credentials: true,
-  // })
   cors({
-    origin: "https://mern-scheduler-frontend.vercel.app/",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
